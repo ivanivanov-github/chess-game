cf8d42e9212a229969a9d94f1c6d121cfb0ed3ad
ivanivanov-gitlab <ivan.ivanov@polymtl.ca>
2024-12-18 19:43:01 -0500

    formatting

--- board.cpp
+++ board.cpp
@@ -1,5 +1,5 @@
 #include "board.h"
 
-Board::Board(std::vector<std::vector<Square>> &squares) : m_squares(squares)
+Board::Board(std::vector<std::vector<Square>>& squares) : m_squares(squares)
 {
 }
